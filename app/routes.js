bdff256b204cc998e167707eb7d39723fe606a10
cwang <cwang100@illinois.edu>
2017-10-20 13:30:45 -0500

    fix indentations

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@ module.exports = function(app, passport) {
   // HOME PAGE (with login links) ========
   // =====================================
   app.get('/', function(req, res) {
-      res.render('index.ejs'); // load the index.ejs file
+    res.render('index.ejs'); // load the index.ejs file
   });
 
   // =====================================
@@ -11,8 +11,8 @@ module.exports = function(app, passport) {
   // =====================================
   app.get('/login', function(req, res) {
 
-      // render the page and pass in any flash data if it exists
-      res.render('login.ejs', { message: req.flash('loginMessage') }); 
+    // render the page and pass in any flash data if it exists
+    res.render('login.ejs', { message: req.flash('loginMessage') }); 
   });
 
   // =====================================
@@ -20,8 +20,8 @@ module.exports = function(app, passport) {
   // =====================================
   app.get('/signup', function(req, res) {
 
-      // render the page and pass in any flash data if it exists
-      res.render('signup.ejs', { message: req.flash('signupMessage') });
+    // render the page and pass in any flash data if it exists
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
   });
 
   // process the signup form
@@ -33,27 +33,27 @@ module.exports = function(app, passport) {
   // we will want this protected so you have to be logged in to visit
   // we will use route middleware to verify this (the isLoggedIn function)
   app.get('/profile', isLoggedIn, function(req, res) {
-      res.render('profile.ejs', {
-          user : req.user // get the user out of session and pass to template
-      });
+    res.render('profile.ejs', {
+        user : req.user // get the user out of session and pass to template
+    });
   });
 
   // =====================================
   // LOGOUT ==============================
   // =====================================
   app.get('/logout', function(req, res) {
-      req.logout();
-      res.redirect('/');
+    req.logout();
+    res.redirect('/');
   });
 };
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
 
-    // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated())
-        return next();
+  // if user is authenticated in the session, carry on 
+  if (req.isAuthenticated())
+    return next();
 
-    // if they aren't redirect them to the home page
-    res.redirect('/');
+  // if they aren't redirect them to the home page
+  res.redirect('/');
 }
